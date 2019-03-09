42cec2cf96e35352664cf5d9e5136fed46a3e675
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-09 17:40:46 +0700

    fix slot function in point table

--- src/components/PointTable.vue
+++ src/components/PointTable.vue
@@ -1,6 +1,6 @@
 <template>
 <div ref="tableWrapper" @scroll="horizontalScroll">
-  <table ref="mainTable" class="table is-fullwidth">
+  <table ref="mainTable">
     <thead ref="tableHead">
       <slot name="p-head">
         <tr v-if="mutableBody">
@@ -13,7 +13,10 @@
     <thead ref="stickyTableHead" class="table-head-fixed">
       <slot name="p-sticky-head"></slot>
     </thead>
-    <tbody>
+    <thead ref="stickyBgTableHead" class="table-bg-head-fixed">
+      <slot name="p-sticky-bg-head"></slot>
+    </thead>
+    <tbody ref="tableBody">
       <slot name="p-body">
         <tr v-for="(data, indexData) in mutableBody" :key="indexData">
           <template v-for="(key, indexKey) in Object.keys(data)">
@@ -46,32 +49,41 @@ export default {
       tableHeadHeight: 0,
       anchorTop: 0,
       anchorBottom: 0,
-      tableHeight: 0,
       mutableHead: this.header,
       mutableBody: this.data
     }
   },
   computed: {
+    showHeaderFixed () {
+      if (this.anchorTop <= 0 && this.anchorBottom >= 0) {
+        return true
+      } else {
+        return false
+      }
+    }
   },
   methods: {
     horizontalScroll () {
-      // sync sticky table head with original table head
-      this.$refs.stickyTableHead.style.left = this.$refs.mainTable.getBoundingClientRect().x + 'px'
-      var bodyX = this.$slots['p-body'][0].children[0].elm.getBoundingClientRect().x;
-      // remove fixed head that out of table area
-      for (var i = 0; i < this.$slots['p-sticky-head'][0].children.length; i++) {
-        var thX = this.$refs.mainTable.children[1].children[0].children[i].getBoundingClientRect().x
-        var thWidth = this.$refs.stickyTableHead.children[0].children[i].clientWidth
-        // ignore sticky th
-        if (i > 0) {
-          // remove fixed head that out of table area
-          if (thX  + thWidth > bodyX + this.$refs.tableWrapper.clientWidth) {
-            // hide right column
-            this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'hidden'
-          } else if (thX <= bodyX || this.anchorTop > 0 || this.anchorBottom < 0) {
-            // hide left column
-            this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'hidden'
+      if (this.showHeaderFixed) {
+        // sync sticky table head with original table head
+        this.$refs.stickyTableHead.style.left = this.$refs.mainTable.getBoundingClientRect().x + 'px'
+        var bodyX = this.$refs.tableBody.children[0].children[0].getBoundingClientRect().x;
+        var wrapperX = this.$refs.tableWrapper.getBoundingClientRect().x
+        var wrapperWidth = this.$refs.tableWrapper.clientWidth
+        // remove fixed head that out of table area
+        for (var i = 0; i < this.$refs.stickyTableHead.children[0].children.length; i++) {
+          var thX = this.$refs.stickyTableHead.children[0].children[i].getBoundingClientRect().x
+          var thWidth = this.$refs.stickyTableHead.children[0].children[i].clientWidth
+          // ignore sticky th
+          if (i > 0) {
+            // hide column if outside wrapper width
+            if (thX <= bodyX || thX + thWidth >= wrapperWidth + wrapperX) {
+              this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'hidden'
+            } else {
+              this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'visible'
+            }
           } else {
+            // freeze column always visible
             this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'visible'
           }
         }
@@ -81,35 +93,54 @@ export default {
       var scroll = window.scrollY
       this.anchorTop = this.$refs.mainTable.getBoundingClientRect().y
       this.anchorBottom = this.$refs.anchorBottom.getBoundingClientRect().y
-      if (this.anchorTop <= 0 && this.anchorBottom >= 0) {
-        this.$refs.stickyTableHead.style.visibility = 'visible'
+      if (this.showHeaderFixed) {
+        this.$refs.stickyTableHead.style.display = ''
+        this.$refs.stickyBgTableHead.style.display = ''
       } else {
-        this.$refs.stickyTableHead.style.visibility = 'hidden'
+        this.$refs.stickyTableHead.style.display = 'none'
+        this.$refs.stickyBgTableHead.style.display = 'none'
       }
     }
   },
-  mounted () {
+  mounted () {    
     window.addEventListener('scroll', this.verticalScroll)
     window.addEventListener('scroll', this.horizontalScroll)
     window.addEventListener('resize', this.verticalScroll)
     window.addEventListener('resize', this.horizontalScroll)
-    this.$refs.stickyTableHead.style.visibility = 'hidden'    
+
     this.$refs.stickyTableHead.style.width = this.$refs.mainTable.clientWidth + 'px'
-    for (var i = 0; i < this.$slots['p-head'][0].children.length; i++) {
-      this.$refs.stickyTableHead.children[0].children[i].style.width = this.$slots['p-head'][0].children[i].elm.clientWidth +'px'
+    this.$refs.stickyBgTableHead.style.width = this.$refs.tableWrapper.clientWidth + 'px'
+    for (var i = 0; i < this.$refs.tableHead.children[0].children.length; i++) {
+      this.$refs.stickyTableHead.children[0].children[i].style.width = this.$refs.tableHead.children[0].children[i].clientWidth + 'px'
+      this.$refs.stickyBgTableHead.children[0].children[i].style.width = this.$refs.tableHead.children[0].children[i].clientWidth + 'px'
+      // add background for hidden head
+      this.$refs.stickyBgTableHead.children[0].children[i].style.color = '#fff'
+      this.$refs.stickyBgTableHead.children[0].children[i].style.background = '#fff'
+      
+      var wrapperWidth = this.$refs.tableWrapper.clientWidth
+      var thX = this.$refs.stickyTableHead.children[0].children[i].getBoundingClientRect().x
+      var thWidth = this.$refs.stickyTableHead.children[0].children[i].clientWidth
+      if (thX + thWidth > wrapperWidth) {
+        this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'hidden'
+      } else {
+        this.$refs.stickyTableHead.children[0].children[i].style.visibility = 'visible'
+      }
     }
+    this.$refs.stickyTableHead.style.display = 'none'
+    this.$refs.stickyBgTableHead.style.display = 'none'
   },
   created () {
     // clone table head into table sticky head
-    this.$slots['p-sticky-head'] = this.$slots['p-head']
+    if (this.$slots['p-head']) {
+      this.$slots['p-sticky-head'] = this.$slots['p-head']
+      this.$slots['p-sticky-bg-head'] = this.$slots['p-head']
+    }
   },
   destroyed () {
-    window.removeEventListener('scroll', this.verticalScroll)
     window.removeEventListener('scroll', this.horizontalScroll)
-    window.removeEventListener('resize', this.verticalScroll)
+    window.removeEventListener('scroll', this.verticalScroll)
     window.removeEventListener('resize', this.horizontalScroll)
-  },
-  updated () {
+    window.removeEventListener('resize', this.verticalScroll)
   }
 }
 </script>
@@ -138,17 +169,21 @@ thead th:first-child {
   z-index: 1;
   left: 0;
 }
-.table-head-fixed {
+.table-bg-head-fixed {
+  position: fixed;
+  top: 0;
   z-index: 2;
+}
+.table-head-fixed {
+  position: fixed;
+  top: 0;
+  z-index: 3;
 }
 tbody th {
   position: -webkit-sticky; /* for Safari */
   position: sticky;
   left: 0;
-  background: #FFF;
+  background: #fff;
 }
-.table-head-fixed {
-  position: fixed;
-  top: 0;
-}
+
 </style>
